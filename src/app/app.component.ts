1a9ea88361d1f5bfa2d3256b5485f52a2f33d20d
Keiler <keilermora@gmail.com>
2020-09-30 00:34:44 -0300

    al navegar entre rutas, la aplicación va a hacer scroll al inicio de la vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pokedex';
+export class AppComponent implements OnInit {
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      document.body.scrollTop = 0;
+      window.scrollTo(0, 0);
+  });
+  }
 }
