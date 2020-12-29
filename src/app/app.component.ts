cf1e4c48a6d29f1be30bce6cdac73ee90bc9723e
Keiler Mora <keilermora@gmail.com>
2020-12-29 17:25:25 -0300

    agrega pagina 404.html para que gihub pages redireccione al home y sea la aplicación quieen decida si la ruta existe o no

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
+
+    // Si existe una ruta en el localStorage, navegar a ella.
+    // Esto ocurre cuando el servidor muestra la pagina 404 sin saber que se
+    // trata de una single page application.
+    const path = localStorage.getItem('path');
+    if(path) {
+      localStorage.removeItem('path');
+      this.router.navigate([path]);
+    }
+
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
         return;
@@ -20,6 +30,6 @@ export class AppComponent implements OnInit {
         document.body.scrollTop = 0;
         window.scrollTo(0, 0);
       }
-  });
+    });
   }
 }
