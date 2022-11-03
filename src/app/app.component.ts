dee3633e0fff4dd70ca7e3db5ef0ae1c34acdc01
Keiler Mora <keilermora@gmail.com>
2022-11-03 20:35:04 -0300

    feat: add automatic swupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Event, Router, Scroll } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 import { filter } from 'rxjs/operators';
-import Pokedex from 'src/app/core/services/filter/filter.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,31 +11,41 @@ import Pokedex from 'src/app/core/services/filter/filter.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  pokedex: Pokedex = {} as Pokedex;
-
-  constructor(private router: Router, private viewportScroller: ViewportScroller) {}
+  constructor(
+    private router: Router,
+    private swUpdate: SwUpdate,
+    private viewportScroller: ViewportScroller
+  ) {}
 
   ngOnInit() {
-    // Si existe una ruta en el localStorage, navegar a ella.
-    // Esto ocurre cuando el servidor muestra la pagina 404 sin saber que se
-    // trata de una single page application.
-    const route = localStorage.getItem('angular-route');
-    if (route) {
-      localStorage.removeItem('angular-route');
-      this.router.navigate([route]);
-    }
+    this.checkPWAUpdate();
+    this.setScrollBehaviour();
+  }
 
+  private checkPWAUpdate() {
+    if (!this.swUpdate.isEnabled) return;
+
+    this.swUpdate.checkForUpdate().then((hasUpdate) => {
+      if (hasUpdate) {
+        this.swUpdate.activateUpdate().then(() => {
+          location.reload();
+        });
+      }
+    });
+  }
+
+  private setScrollBehaviour() {
     this.router.events
       .pipe(filter((e: Event): e is Scroll => e instanceof Scroll))
       .subscribe((e: Scroll) => {
         if (e.position) {
-          // backward navigation
+          // Backward navigation
           this.viewportScroller.scrollToPosition(e.position);
         } else if (e.anchor) {
-          // anchor navigation
+          // Anchor navigation
           this.viewportScroller.scrollToAnchor(e.anchor);
         } else if (!/\?/.test(e.routerEvent.url)) {
-          // forward navigation
+          // Forward navigation
           this.viewportScroller.scrollToPosition([0, 0]);
         }
       });
